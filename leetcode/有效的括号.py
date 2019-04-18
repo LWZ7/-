8876c1b7601f1924fce587149b89a0103d55c942
LWZ <30646654+LWZ7@users.noreply.github.com>
2019-04-18 20:38:34 +0800

    Create 有效的括号.py

---
+++ leetcode/有效的括号.py
@@ -0,0 +1,72 @@
+'''
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
+
+示例 1:
+
+输入: "()"
+输出: true
+示例 2:
+
+输入: "()[]{}"
+输出: true
+示例 3:
+
+输入: "(]"
+输出: false
+示例 4:
+
+输入: "([)]"
+输出: false
+示例 5:
+
+输入: "{[]}"
+输出: true
+'''
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s)==0:
+            return True
+        left = []
+        right = []
+        for i in s:
+            if i=="(" or i=="[" or i=="{":
+                left.append(i)
+            if i=="}" or i=="]" or i==")":
+                if i=="}":
+                    if len(left)==0:
+                        return False
+                    else:
+                        if left[-1]=="{":
+                            left.pop()
+                        else:
+                            return False
+                elif i=="]":
+                    if len(left)==0:
+                        return False
+                    else:
+                        if left[-1]=="[":
+                            left.pop()
+                        else:
+                            return False
+                elif  i==")":
+                    if len(left)==0:
+                        return False
+                    else:
+                        if left[-1]=="(":
+                            left.pop()
+                        else:
+                            return False
+        if len(left)!=0:
+            return False
+        else:
+            return True
