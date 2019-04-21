adf937be16b4908e8755f906fed9f8b854dd0d8b
LWZ <30646654+LWZ7@users.noreply.github.com>
2019-04-21 21:14:27 +0800

    Create 二叉树的中序遍历.py

---
+++ leetcode/二叉树的中序遍历.py
@@ -0,0 +1,38 @@
+'''
+给定一个二叉树，返回它的中序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        list1 = []
+        if not root:
+            return res
+        
+        while root or len(list1)>0:
+            if root:
+                list1.append(root)
+                root = root.left
+            else:
+                root = list1.pop()
+                res.append(root.val)
+                root = root.right
+        return res
