73cfee218fdae7873c38429d1a3d7ddea3af7b73
LWZ <30646654+LWZ7@users.noreply.github.com>
2019-05-03 16:30:35 +0800

    Create 换零钱.py

---
+++ leetcode/动态规划/换零钱.py
@@ -0,0 +1,27 @@
+'''
+给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+
+示例 1:
+
+输入: coins = [1, 2, 5], amount = 11
+输出: 3 
+解释: 11 = 5 + 5 + 1
+示例 2:
+
+输入: coins = [2], amount = 3
+输出: -1
+
+分别用动态规划、深度优先、广度优先解决
+'''
+#动态规划
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        mem = [float('inf')]*(amount+1)
+        mem[0] = 0
+        
+        for coin in coins:
+            for j in range(coin, amount+1):
+                if j-coin >= 0:
+                    mem[j] = min(mem[j], mem[j - coin] + 1)
+                
+        return -1 if mem[-1] > amount else mem[-1]
