6c811bba63d7ba7b2f29706f415b613f7e1cf584
LWZ <30646654+LWZ7@users.noreply.github.com>
2019-05-06 17:13:58 +0800

    Update 合并二叉树.py

--- leetcode/树/合并二叉树.py
+++ leetcode/树/合并二叉树.py
@@ -60,3 +60,12 @@ class Solution:
         if not t1 and t2:
             return t2
         return self.helper(t1 , t2)
+
+######简洁版######
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if t1 and t2 :
+            t1.val += t2.val
+            t1.left = self.mergeTrees(t1.left, t2.left)
+            t1.right = self.mergeTrees(t1.right, t2.right)
+            return t1
+        return t1 or t2
