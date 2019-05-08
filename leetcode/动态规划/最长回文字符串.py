2b931a0f04261d7604b7975aeaed9c0e31d83c10
LWZ <30646654+LWZ7@users.noreply.github.com>
2019-05-08 12:22:02 +0800

    Update 最长回文字符串.py

--- leetcode/动态规划/最长回文字符串.py
+++ leetcode/动态规划/最长回文字符串.py
@@ -12,35 +12,23 @@
 输出: "bb"
 '''
 class Solution:
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        k= len(s)
-        matrix = [[0 for i in range(k)] for i in range(len(s))]
-        logestSubStr = ""
-        # 存储最长回文子串
-        logestLen = 0
-        # 最长回文子串的长度
-
-        for j in range(0, len(s)):
-            for i in range(0, j+1):
-                if j - i <= 1:
-                    if s[i] == s[j]:
-                        matrix[i][j] = 1
-                        # 此时f(i,j)置为true
-                        if logestLen < j - i + 1:
-                            # 将s[i:j]的长度与当前的回文子串的最长长度相比 
-                            logestSubStr = s[i:j+1]
-                            # 取当前的最长回文子串
-                            logestLen = j - i + 1
-                            # 当前最长回文子串的长度
-                else:
-                    if s[i] == s[j] and matrix[i+1][j-1]:
-                        # 判断
-                        matrix[i][j] = 1
-                        if logestLen < j - i + 1:
-                            logestSubStr = s[i:j+1]
-                            logestLen = j - i + 1
-        return logestSubStr
+    def longestPalindrome(self, s: str) -> str:
+        if not s :
+            return ""
+        res = ""
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        max_len = float("-inf")
+        #print(dp)
+        for i in range(n):
+            #外循环顺向遍历,内循环逆向遍历
+            for j in range(i,-1,-1):
+                if s[i] == s[j] and (i - j < 2 or dp[i-1][j+1]):
+                    #两个字符和一个字符直接判定，不用看前一个dp
+                    dp[i][j] = 1
+                if dp[i][j] and  max_len < i - j + 1:
+                    #如果这是一个回文串，则判定它的长度
+                    #print("i,j",i,j)
+                    res = s[j:i+1]
+                    max_len = i - j + 1
+        return res 
