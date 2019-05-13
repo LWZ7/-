dad4ebd98ff5e2d7e152f7979a29b2a751c9c531
LWZ <30646654+LWZ7@users.noreply.github.com>
2019-05-13 15:17:17 +0800

    Update 子集.py

--- leetcode/回溯/子集.py
+++ leetcode/回溯/子集.py
@@ -1,1 +1,35 @@
+'''
+给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
 
+说明：解集不能包含重复的子集。
+
+示例:
+
+输入: nums = [1,2,3]
+输出:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+'''
+class Solution: 
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        res = []
+        n = len(nums)
+        
+        def helper(idx, temp_list):
+            res.append(temp_list)
+            for i in range(idx, n):
+                helper(i + 1, temp_list + [nums[i]])
+        
+        helper(0, [])
+        return res
+#这种题一看就知道可以用回溯，但是怎么回溯就是一个比较难的问题，遍历的顺序要在所有情况都合适才行        
