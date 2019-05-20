19dfc25765e3f211f4a1c84280235dd193503455
LWZ <30646654+LWZ7@users.noreply.github.com>
2019-05-20 17:32:19 +0800

    Create 回文链表.py

---
+++ leetcode/链表/回文链表.py
@@ -0,0 +1,41 @@
+'''
+请判断一个链表是否为回文链表。
+
+示例 1:
+
+输入: 1->2
+输出: false
+示例 2:
+
+输入: 1->2->2->1
+输出: true
+进阶：
+你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if not head or not head.next:
+            return True
+        slow = head
+        fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        result = None
+        while slow:
+            temp = slow.next
+            slow.next = result
+            result = slow
+            slow = temp
+        while head:
+            if head.val!=result.val:
+                return False
+            head = head.next
+            result = result.next
+        return True
