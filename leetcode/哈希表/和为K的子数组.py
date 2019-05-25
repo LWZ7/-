c8c545d2572b110090b290af69d56302ba62f0fc
LWZ <30646654+LWZ7@users.noreply.github.com>
2019-05-25 12:34:55 +0800

    Create 和为K的子数组.py

---
+++ leetcode/哈希表/和为K的子数组.py
@@ -0,0 +1,25 @@
+'''
+给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+
+示例 1 :
+
+输入:nums = [1,1,1], k = 2
+输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+说明 :
+
+数组的长度为 [1, 20,000]。
+数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。
+'''
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        dic = {}
+        dic[0] = 1
+        sum = 0
+        ret = 0
+        for i in range(len(nums)):
+            sum+=nums[i]
+            if sum-k in dic:
+                ret+=dic[sum-k]
+            dic[sum] = dic.get(sum,0)+1
+        return ret
+# 扫描一遍数组, 使用map记录出现同样的和的次数, 对每个i计算累计和sum并判断map内是否有sum-k
