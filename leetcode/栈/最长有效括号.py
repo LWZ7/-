455428ea795c4985df677c7530580c43dac58191
LWZ <30646654+LWZ7@users.noreply.github.com>
2019-05-26 20:30:11 +0800

    Create 最长有效括号.py

---
+++ leetcode/栈/最长有效括号.py
@@ -0,0 +1,31 @@
+'''
+给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+
+示例 1:
+
+输入: "(()"
+输出: 2
+解释: 最长有效括号子串为 "()"
+示例 2:
+
+输入: ")()())"
+输出: 4
+解释: 最长有效括号子串为 "()()"
+'''
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        if not s:
+            return 0
+        res = 0
+        stack = [-1]
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    res = max(res,i - stack[-1])
+        return res
+#不在栈中存入括号，而是存入字符的索引
